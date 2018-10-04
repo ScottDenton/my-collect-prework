7549d88c3d3cfaa3c95d7c997f40dd1f56a8d863
scottyd_14 <scottyd_14@hotmail.com>
2018-10-04 22:17:46 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect (collection)
   i = 0 
-
+new_array = []
   while i < collection.length 
-  yield collection[i]
+  new_array << yield collection[i]
   i += 1
   
 end 
-
+new_array
 end
 
